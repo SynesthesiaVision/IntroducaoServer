b1b8c9cbcd9bccfc2e68c563186e56d21b67b966
Carol Medeiros <77502177+mecarolme@users.noreply.github.com>
2024-05-10 09:59:21 -0300

    implementado add ao carrinho

--- assets/js/mainSolucao.js
+++ assets/js/mainSolucao.js
@@ -1,3 +1,22 @@
+// modal no html
+var openModalBtnItem = document.getElementById("openModalBtnItem");
+var modal = document.getElementById("myModal");
+var closeBtn = document.getElementsByClassName("close")[0];
+
+openModalBtnItem.onclick = function () {
+  modal.style.display = "block";
+};
+
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
 // form pra fazer post dos itens
 var formCadastro = document.getElementById("criarItem");
 
@@ -27,30 +46,45 @@ formCadastro.addEventListener("submit", function (e) {
     .catch((error) => console.error("Error:", error));
 });
 
-// form pra fazer get dos itens
-function getItemsFromServer() {
-  fetch("http://localhost:8080/item/all")
-    .then((response) => response.json())
-    .then((data) => {
-      var container = document.getElementById("container-itens");
+// add itens criados ao carrinho
+let itensCarrinho = [];
+let quantidadeTotalItens = 0;
 
-      data.forEach((item) => {
-        var itemDiv = document.createElement("div");
-        itemDiv.classList.add("item");
-
-        itemDiv.innerHTML = `
-        <p>Item ID: ${item.id}</p>
-        <p>Nome: ${item.name}</p>
-        <p>Preço: ${item.price}</p>
-        <hr>
-      `;
-
-        container.appendChild(itemDiv);
-      });
-    })
-    .catch((error) => {
-      console.error("Erro ao obter o nome do usuário:", error);
-    });
+function limparCarrinho() {
+  const containerCarrinho = document.getElementById("container-carrinho");
+  containerCarrinho.innerHTML = "";
 }
 
-document.addEventListener("DOMContentLoaded", getItemsFromServer);
+function adicionarItemCarrinho(itemId, nomeItem, precoItem) {
+  const index = itensCarrinho.findIndex((item) => item.id === itemId);
+  if (index !== -1) {
+    itensCarrinho[index].quantidade++;
+  } else {
+    itensCarrinho.push({
+      id: itemId,
+      nome: nomeItem,
+      preco: precoItem,
+      quantidade: 1,
+    });
+  }
+
+  quantidadeTotalItens++;
+
+  document.getElementById("quantidade-total-itens").textContent =
+    quantidadeTotalItens;
+}
+
+// função que vai realmente adicionar os itens ao carrinho (depende das 2 funções anteriores)
+function carrinho(itemId, nomeItem, precoItem) {
+  adicionarItemCarrinho(itemId, nomeItem, precoItem);
+
+  limparCarrinho();
+
+  itensCarrinho.forEach((item) => {
+    const itemElement = document.createElement("div");
+    itemElement.textContent = `${item.nome} - Quantidade: ${
+      item.quantidade
+    } - Preço Total: R$${item.preco * item.quantidade}`;
+    document.getElementById("container-carrinho").appendChild(itemElement);
+  });
+}
